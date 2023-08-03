7a3b26b5740b99bce51d76d48085781b9bd217d2
Aiden Samuels <aide3ndev@gmail.com>
2023-08-03 14:53:41 +0100

    added api route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -32,6 +32,10 @@ pieces = [
 @app.route("/")
 def index():
     return render_template("home.html", pieces= pieces, company_name= "Vane")
+
+@app.route("/api/pieces")
+def list_pieces():
+    return jsonify(pieces)
 
 if __name__ == "__main__":
     app.run(debug=True)
