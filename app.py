f4d84eab0de510d122665887a6e522c9bd0130d8
Aiden Samuels <aide3ndev@gmail.com>
2023-08-03 11:50:15 +0100

    more styling new template

--- app.py
+++ app.py
@@ -2,9 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+pieces = [
+    {
+        'name':'Punk Earings',
+        'type': 'Earings',
+        'price': '$$$',
+        'color': 'Silver'
+    },
+    {
+        'name':'Chained Heart Ring',
+        'type': 'Ring',
+        'price': '$$$',
+        'color': 'Silver'
+    },
+    {
+        'name':'Amorphous Ring',
+        'type': 'Ring',
+        'price': '$$$',
+        'color': 'Silver'
+    },
+    {
+        'name':'Spiked Necklace',
+        'type': 'Necklace',
+        'price': '$$$',
+        'color': 'Silver'
+    }
+]
+
 @app.route("/")
 def index():
-    return render_template("home.html")
+    return render_template("home.html", pieces= pieces, company_name= "Vane")
 
 if __name__ == "__main__":
     app.run(debug=True)
